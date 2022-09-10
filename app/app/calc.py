b0fd97115d852dbf39486b37b8e6bccbc110d4b8
zkhan12 <zoheb1112@gmail.com>
2022-09-10 02:35:49 -0500

    add db dependencies

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+'''Calculator functions'''
+
+
+def add(x, y):
+    '''add x and y and return the result'''
+    return x+y
+
+
+def subtract(x, y):
+    '''subtract y from x and return result'''
+    return x-y
